e78e2e42c5bbe8509a4af872963b6288688f18f8
MD-AHAD-KHAN-PATHAN <md.ahadkhanpathan.tct@gmail.com>
2023-11-26 19:26:26 +0600

    added all users page data

--- src/Pages/DashBoards/AdminDashBoard/ManageUsers/ManageUsers.jsx
+++ src/Pages/DashBoards/AdminDashBoard/ManageUsers/ManageUsers.jsx
@@ -1,8 +1,62 @@
 
+
+import useAllUsers from "../../../../Hooks/useAllUsers";
+
 const ManageUsers = () => {
+
+    const [users, refetch] = useAllUsers();
+    
+
     return (
-        <div>
-            <h1>this is admin Manage Users page</h1>
+        <div className="overflow-x-auto m-20">
+            <h1 className="text-5xl font-bold text-lime-400 text-center mb-8">All Users</h1>
+            <table className="table">
+                {/* head */}
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Picture</th>
+                        <th>Admin Role</th>
+                        <th>Agent Role</th>
+                        <th>Fraud</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        users.map((user, indx) => <tr key={user?._id}>
+                        <th>
+                            {indx + 1}
+                        </th>
+                        <td>
+                            <div className="flex items-center gap-3">
+                                <div className="avatar">
+                                    <div className="mask mask-squircle w-12 h-12">
+                                        <img src={user?.photo} alt="Avatar Tailwind CSS Component" />
+                                    </div>
+                                </div>
+                                <div>
+                                    <div className="font-bold">{user?.name}</div>
+                                    <div className="text-sm opacity-50">{user?.email}</div>
+                                </div>
+                            </div>
+                        </td>
+                        <td>
+                            <button className="bg-green-400 text-white p-2 rounded-full font-semibold">Make Admin</button>
+                        </td>
+
+                        <td><button className="bg-yellow-300 text-white p-2 rounded-full font-semibold">Make Agent</button></td>
+                        <td>
+                            <button className="bg-red-400 text-white p-2 rounded-full font-semibold">Make Fraud</button>
+                        </td>
+                        <td>
+                            <button className="bg-red-700 text-white p-2 rounded-full font-semibold">Delete user</button>
+                        </td>
+                    </tr>)
+                    }
+                    
+                </tbody>
+            </table>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,20 @@ import { RouterProvider } from 'react-router-dom'
 import Routes from './Routes/Routes'
 import AuthProvider from './AuthProvider/AuthProvider'
 
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={Routes}></RouterProvider>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={Routes}></RouterProvider>
+      </AuthProvider>
+    </QueryClientProvider>
+
   </React.StrictMode>,
 )
